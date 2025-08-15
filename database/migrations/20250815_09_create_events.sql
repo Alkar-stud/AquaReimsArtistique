b1e65bfa00ec96643b6a793b364b09db472a4eaa
Alkar-stud <alexandre.kapps@gmail.com>
2025-08-15 10:07:44 +0200

    Create SQL tables for events

---
+++ database/migrations/20250815_09_create_events.sql
@@ -0,0 +1,13 @@
+-- Création de la table events
+CREATE TABLE IF NOT EXISTS `events` (
+    `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `libelle` varchar(255) NOT NULL,
+    `lieu` bigint NOT NULL,
+    FOREIGN KEY (`lieu`) REFERENCES `piscines`(`id`),
+    `opening_doors_at` DATETIME NOT NULL,
+    `event_start_at` DATETIME NOT NULL,
+    `associate_event` bigint NULL,
+    FOREIGN KEY (`associate_event`) REFERENCES `events`(`id`),
+    `created_at` DATETIME NOT NULL,
+    `updated_at` DATETIME NULL
+);
---
+++ database/migrations/20250815_10_create_events_inscriptions_dates.sql
@@ -0,0 +1,13 @@
+-- Création de la table events_inscriptions_dates.
+CREATE TABLE IF NOT EXISTS `events_inscriptions_dates.`
+(
+    `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `event` bigint NOT NULL,
+    FOREIGN KEY (`event`) REFERENCES `events`(`id`),
+    `libelle` char(64) NOT NULL,
+    `start_registration_at` DATETIME NOT NULL,
+    `close_registration_at` DATETIME NOT NULL,
+    `access_code` char(24) NULL,
+    `created_at` DATETIME NOT NULL,
+    `updated_at` DATETIME NULL
+);

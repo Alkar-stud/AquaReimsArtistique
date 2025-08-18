0feb647e839df5407c9fabd65a157f36d0e36b76
Alkar-stud <alexandre.kapps@gmail.com>
2025-08-18 13:06:38 +0200

    Add reservations_mails_sent table, model and repository

--- database/migrations/20250815_11_create_events_tarifs.sql
+++ database/migrations/20250815_11_create_events_tarifs.sql
@@ -1,6 +1,5 @@
 -- Création de la table events_tarifs
-CREATE TABLE IF NOT EXISTS `events_tarifs`
-(
+CREATE TABLE IF NOT EXISTS `events_tarifs` (
     `event` bigint NOT NULL,
     FOREIGN KEY (`event`) REFERENCES `events`(`id`),
     `tarif` bigint NOT NULL,
---
+++ database/migrations/20250818_13_create_reservations_mails_sent.sql
@@ -0,0 +1,10 @@
+-- Création de la table reservations_mails_sent
+CREATE TABLE IF NOT EXISTS `reservations_mails_sent`
+(
+    `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `reservation` bigint NOT NULL,
+    FOREIGN KEY (`reservation`) REFERENCES `reservations`(`id`),
+    `mail_template` bigint NOT NULL,
+    FOREIGN KEY (`mail_template`) REFERENCES `mails_templates`(`id`),
+    `sent_at` DATETIME NOT NULL
+);

cac585ce63b71de39229bd5c482f6753211037e0
Alkar-stud <alexandre.kapps@gmail.com>
2025-10-11 18:04:04 +0200

    Enhance event presentation process: add service to link images to reservation

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,24 +3,32 @@ namespace app\Controllers;
 
 use app\Attributes\Route;
 use app\Repository\Event\EventPresentationsRepository;
-use DOMDocument;
-use Exception;
-
-#[Route('/', name: 'app_home')]
+use app\Services\Event\EventPresentationService;
 
 class HomeController extends AbstractController
 {
-    public function __construct()
+    private EventPresentationsRepository $eventPresentationsRepository;
+    private EventPresentationService $eventPresentationService;
+
+    public function __construct(
+        EventPresentationsRepository $eventPresentationsRepository,
+        EventPresentationService $eventPresentationService,
+    )
     {
         parent::__construct(true); // true = route publique, pas de vérif session pour éviter le TOO_MANY_REDIRECT
+        $this->eventPresentationsRepository = $eventPresentationsRepository;
+        $this->eventPresentationService = $eventPresentationService;
     }
 
-    /**
-     * @throws Exception
-     */
+    #[Route('/', name: 'app_home')]
     public function index(): void
     {
+        $eventPresentations = $this->eventPresentationsRepository->findAll(true, true);
 
-        $this->render('home', [], 'Accueil');
+        $contents = $this->eventPresentationService->addLinkToAllPictures($eventPresentations);
+
+        $this->render('home', [
+            'contents' => $contents
+        ], 'Accueil');
     }
 }

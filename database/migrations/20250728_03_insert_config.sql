3a62f22cb57069560c80b40f2a168223831fb873
Alkar-stud <alexandre.kapps@gmail.com>
2025-10-15 09:59:02 +0200

    Enhance configuration settings: add maximum donation percentage for slider

--- database/migrations/20250728_03_insert_config.sql
+++ database/migrations/20250728_03_insert_config.sql
@@ -11,4 +11,5 @@ INSERT IGNORE INTO `config` (`label`, `config_key`, `config_value`, `config_type
 ('Taille maxi des fichiers (Mo) justificatif pour preuve de tarif', 'MAX_UPLOAD_PROOF_SIZE', '4', 'int'),
 ('Token pour cron', 'ACCESS_TOKEN', 'ToeknAGenerer', 'string'),
 ('nb caractère pour token', 'NB_CARACTERE_TOKEN', '32', 'int'),
-('Signature des mails', 'SIGNATURE', 'Aqua Reims Artistique vous remercie et à bientôt...', 'string');
+('Signature des mails', 'SIGNATURE', 'Aqua Reims Artistique vous remercie et à bientôt...', 'string'),
+('% max de la commande pour la jauge de don', 'DONATION_SLIDER_MAX_PERCENTAGE', '50', 'int');

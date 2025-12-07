2977653f86e328fe4d8ad09f7b5870df331f0ce5
Alkar-stud <alexandre.kapps@gmail.com>
2025-12-07 18:19:20 +0100

    Add RGPD consent field to reservations tables: update schema for consent tracking

--- database/migrations/20251207_01_alter_table_reservations.sql
+++ database/migrations/20251207_01_alter_table_reservations.sql
@@ -1,8 +1,8 @@
 -- Modification de la table reservation
 ALTER TABLE
-    `reservation` ADD `rgpd_consent` datetime NULL DEFAULT NULL AFTER `swimmer_if_limitation`;
+    `reservation` ADD `rgpd_date_consentement` datetime NULL DEFAULT NULL AFTER `swimmer_if_limitation`;
 
 -- Modification de la table reservation_temp
 ALTER TABLE
-    `reservation_temp` ADD `rgpd_consent` datetime NULL DEFAULT NULL AFTER `swimmer_if_limitation`;
+    `reservation_temp` ADD `rgpd_date_consentement` datetime NULL DEFAULT NULL AFTER `swimmer_if_limitation`;
 
